e3b934781da37b6c893bf1d6988eb3f644936bb4
ValeraNazarchuk <valera55555nazarchuk@gmail.com>
2023-02-10 21:42:16 +0200

    ready-made slider operation logic in projects

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -39,3 +39,60 @@ window.onscroll = () => {
     back.classList.remove('back__top-active')
   }
 }
+
+//---------------------Projects_*Slider*-------------------------------
+
+const projectBody = document.querySelector('.projects__body')
+const projectLink = document.querySelectorAll('.projects__body-link')
+
+const btnNext = document.querySelector('.projects-next')
+const btnBack = document.querySelector('.projects-back')
+
+let count = 0
+let width
+
+function sliderToShow(value) {
+  if (value > 640) return 3
+
+  if (value > 490) return 2
+  else return 1
+}
+
+function inits() {
+  const offsetWidth = document.querySelector('.projects__inner').offsetWidth
+  width = offsetWidth / sliderToShow(offsetWidth)
+
+  projectBody.style.width = width * projectLink.length + 'px'
+
+  projectLink.forEach((item) => {
+    item.style.width = width + 'px'
+    item.style.height = 'auto'
+    item.style.margin = '0 auto' // під вопросом
+  })
+  rollSliders()
+}
+
+inits()
+window.addEventListener('resize', inits)
+
+btnNext.addEventListener('click', () => {
+  const offsetWidth = document.querySelector('.projects__inner').offsetWidth
+  count++
+  if (count > projectLink.length - sliderToShow(offsetWidth)) {
+    count = 0
+  }
+  rollSliders()
+})
+
+btnBack.addEventListener('click', () => {
+  count--
+  if (count < 0) {
+    const offsetWidth = document.querySelector('.projects__inner').offsetWidth
+    count = projectLink.length - sliderToShow(offsetWidth)
+  }
+  rollSliders()
+})
+
+function rollSliders() {
+  projectBody.style.transform = `translate(-${count * width}px)`
+}
